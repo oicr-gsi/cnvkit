c2ceb1bf2e5cdc1d4176ad7e2b4bfa3ce483dba8
alkabenawra <alkabenawra@rogers.com>
2018-06-25 15:31:50 -0400

    Fixed handle script

--- workflow-cnvkit/workflow/dependencies/handleFile.sh
+++ workflow-cnvkit/workflow/dependencies/handleFile.sh
@@ -5,23 +5,25 @@ extid=$1
 inPath=$wdPath/$2
 cd $inPath
 model=model-fit; mkdir $model
-EXTT= sample
+EXTT=sample
+
 
 for f in $( ls ); do
-	if [[ -d $f ]]; then
-		if [[ $f != "model-fit" ]]; then
-			cd $inPath
-			mv $f $model
-		fi
-	elif [[ ${f: -20} != ".segmetrics.call.cns" ]]; then	
-                         awk -v j=$EXTT ‘{ if (NR>1) {print j”\t”$1"\t”$2"\t”$3"\t”$7"\t”$5 } }’ $f > $EXTT.cnvkit.seg
-		fi 
-        else 
-            mv $f $model
-            fi 
-	fi
+        if [[ -d $f ]]; then
+                if [[ $f != "model-fit" ]]; then
+                        cd $inPath
+                        mv $f $model
+                fi
+        elif [[ ${f: -20} = ".segmetrics.call.cns" ]]; then
+                        awk -v j=$EXTT '{ if (NR>1) {print j"\t"$1"\t"$2"\t"$3"\t"$7"\t"$5 } }' $f > $EXTT.cnvkit.seg
+               
+        else
+             mv $f $model
+        fi
 done
 
 cd $model
 cd $inPath
 tar -zcvf ${model}.tar.gz $model
+
+
