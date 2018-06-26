c0b40c98fe28f93b6134aa36d6643a47dce3bf36
alkabenawra <alkabenawra@rogers.com>
2018-06-26 12:26:05 -0400

    Changed first line in handlefile.sh

--- workflow-cnvkit/workflow/dependencies/handleFile.sh
+++ workflow-cnvkit/workflow/dependencies/handleFile.sh
@@ -2,24 +2,24 @@
 wdPath=`pwd`/tmp
 model=$wdPath/model-fit;
 mkdir -p $model
+EXTT=sample
 
-
-
-for f in ( `ls $wdPath` ); do
+for f in [ `ls $wdPath` ]; do
         if [[ -d $f ]]; then #check if directory
                 if [[ $f != "model-fit" ]]; then 
                         cd $inPath
                         mv $f $model
                 fi
         elif [[ ${f: -20} = ".segmetrics.call.cns" ]]; then
-                        awk -v j=$EXTT '{ if (NR>1) {print j"\t"$1"\t"$2"\t"$3"\t"$7"\t"$5 } }' $f > $EXTT.cnvkit.seg
-               
+                        awk -v j=$EXTT '{ if (NR>1) {print j"\t"$1"\t"$2"\t"$3"\t"$7"\t"$5 } }' $f > $EXTT.cnvkit.seg 
         else
              mv $f $model
         fi
 done
 
-cd $model
+cd $wdPath 
 tar -zcvf ${model}.tar.gz $model
+
+
 
 
